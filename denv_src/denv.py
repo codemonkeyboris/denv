fb2a7ccc57bb06d0be3b1b66b58995cf83da7c4b
Bohan Zheng <boris.helloworld@gmail.com>
2023-10-17 23:26:42 -0400

    flash working

--- denv_src/denv.py
+++ denv_src/denv.py
@@ -1,39 +1,20 @@
-# #!/usr/bin/python3
-# import sys
-# import os
-# import subprocess
+#!/usr/bin/python3  
+# must be top line
 
-
-# DOCKER_IMAGE = "denv"
-
-# def main():
-#     if len(sys.argv) < 2:
-#         print("Usage: denv.py <command> [arguments...]")
-#         sys.exit(1)
-
-#     command = sys.argv[1]
-#     docker_command = ["sudo", "docker", "run", "-it", "--rm", "-v", f"{os.getcwd()}:/app", DOCKER_IMAGE] + sys.argv[1:]
-
-#     try:
-#         subprocess.run(docker_command, check=True, stderr=subprocess.DEVNULL)
-#     except subprocess.CalledProcessError as e:
-#         if e.returncode == 127:
-#             print(f"Error: Command '{command}' not found in the Docker container.")
-#         sys.exit(1)
-
-# if __name__ == "__main__":
-#     main()
-
-
-#!/usr/bin/python3
 import sys
 import os
 import subprocess
 
-
+DEBUG = False
 DOCKER_IMAGE = "denv"
 
+
+def debug_print(obj): 
+    if DEBUG:
+        print(obj)
+
 def main():
+    debug_print("HELLO DENV!")
     if len(sys.argv) < 2:
         print("Usage: denv.py <command> [arguments...]")
         sys.exit(1)
@@ -41,6 +22,7 @@ def main():
     command = sys.argv[1]
     docker_command = ["sudo", "docker", "run", "-it", "--rm", "--privileged", "--device=/dev:/dev", "-v", f"{os.getcwd()}:/app", DOCKER_IMAGE] + sys.argv[1:]
 
+    debug_print(docker_command)
     try:
         subprocess.run(docker_command, check=True, stderr=subprocess.DEVNULL)
     except subprocess.CalledProcessError as e:
@@ -50,47 +32,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-# #!/usr/bin/python3
-# import sys
-# import os
-# import subprocess
-# import argparse
-
-# DOCKER_IMAGE = "denv"
-
-# def main():
-#     parser = argparse.ArgumentParser(description="Run a command in a Docker container.")
-#     parser.add_argument("command", nargs=argparse.REMAINDER, help="The command to run in the Docker container.")
-#     parser.add_argument("--device", help="The device to pass to the Docker container (e.g., /dev/ttyUSB0).")
-
-#     args = parser.parse_args()
-
-#     if not args.command:
-#         parser.print_help()
-#         sys.exit(1)
-
-#     device_arg = [f"--device={args.device}"] if args.device else []
-    
-#     # Docker run command with device argument
-#     docker_command = [
-#         "sudo", "docker", "run", "-it", "--rm", 
-#         *device_arg,  # Pass the specified device to the container if provided
-#         "-v", f"{os.getcwd()}:/app", DOCKER_IMAGE,
-#         "bash", "-c",
-#         f"ls {args.device} && {' '.join(args.command)}"
-#     ]
-
-#     try:
-#         subprocess.run(docker_command, check=True, stderr=subprocess.DEVNULL)
-#     except subprocess.CalledProcessError as e:
-#         if e.returncode == 127:
-#             print(f"Error: Command '{args.command[0]}' not found in the Docker container.")
-#         sys.exit(1)
-
-# if __name__ == "__main__":
-#     main()
-
